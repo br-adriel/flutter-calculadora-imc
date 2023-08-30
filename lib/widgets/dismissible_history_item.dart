e42b011de276ef1c5ee197c5bebe32f0f3d09c85
Adriel <adriel.fsantos@outlook.com>
2023-08-29 23:13:34 -0300

    Adiciona widget DismissibleHistoryItem

---
+++ lib/widgets/dismissible_history_item.dart
@@ -0,0 +1,50 @@
+import 'package:calculadora_imc/classes/pessoa.dart';
+import 'package:calculadora_imc/widgets/history_item.dart';
+import 'package:flutter/material.dart';
+
+class DismissibleHistoryItem extends StatelessWidget {
+  final Pessoa pessoa;
+  final Future<bool> Function(DismissDirection)? confirmDismiss;
+  final DismissDirection? direction;
+  final void Function(DismissDirection)? onDismissed;
+
+  const DismissibleHistoryItem(this.pessoa,
+      {super.key, this.confirmDismiss, this.direction, this.onDismissed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      direction: direction ?? DismissDirection.endToStart,
+      confirmDismiss: confirmDismiss,
+      background: Container(
+        color: Colors.red[500],
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(
+                Icons.delete_rounded,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+      onDismissed: onDismissed,
+      key: Key(pessoa.id),
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 16),
+        decoration: const BoxDecoration(
+          border: BorderDirectional(
+            bottom: BorderSide(
+              width: 1,
+              color: Colors.black12,
+            ),
+          ),
+        ),
+        child: HistoryItem(pessoa),
+      ),
+    );
+  }
+}

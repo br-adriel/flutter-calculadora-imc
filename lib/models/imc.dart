e113ca4d83d9bc0f55e9de8486e9a9bc28669492
Adriel <adriel.fsantos@outlook.com>
2023-09-04 23:03:21 -0300

    Adiciona modelo IMC

---
+++ lib/models/imc.dart
@@ -0,0 +1,10 @@
+class IMCModel {
+  late final int _id;
+  String nome = "";
+  double altura = 0;
+  double peso = 0;
+
+  IMCModel(this._id, this.altura, this.nome, this.peso);
+
+  int get id => _id;
+}
